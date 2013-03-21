50c2f5ae982da42e8d3d047b6b8ca7958cb0fd01
Luis Daniel <luisdaniel@Luiss-iMac.local>
2013-03-21 02:07:59 -0400

    bunch of stuff

--- app.py
+++ app.py
@@ -14,97 +14,137 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+import pymongo
+import bson
+from mongoengine import *
+import models
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int )
 
+
+# --------- Database Connection ---------
+# MongoDB connection to MongoLab's database
+connect('mydata', host=os.environ.get('MONGOLAB_URI'))
+# app.logger.debug("Connecting to MongoLabs")
+# mongodb_uri = 'mongodb://localhost:27017'
+# db_name = 'heroku_app13757832'
+# print('Connecting to database...')
+# try: 
+# 	MONGOLAB_URI = os.environ.get('MONGOLAB_URI')
+# 	connection = pymongo.Connection(MONGOLAB_URI)
+# 	self.db = connection[db_name]
+# 	print('connection succesful')
+# except:
+# 	print('Error: Unable to connect to database.')
+# 	connection = None
 
 
 class Application(tornado.web.Application):
 	def __init__(self):
 		handlers = [
 			(r"/", MainHandler),
-			(r"/reporte/agregar/", AddReportHandler),
-			(r"/reporte/lugar/", LocationReportHandler),
-			(r"/reporte/fecha/", DateReportHandler),
-			(r"/reporte/doble/", DuplicateReportHandler),
-			(r"/reporte/detalles/", DetailReportHandler),
-			(r"/buscar/", SearchReportHandler),
-			(r"/descargar/", DownloadReportHandler)
+			(r"/add/", AddStudentHandler),
+			(r"/view/", ViewStudentHandler),
+			(r"/edit/([a-zA-Z0-9]{6})", EditStudentHandler),
+			(r"/edit/([a-zA-Z0-9]{24})", EditStudentHandler)
 		]
 		settings = dict(
 			template_path=os.path.join(os.path.dirname(__file__), "templates"),
 			static_path=os.path.join(os.path.dirname(__file__), "static"),
+			ui_modules={"Student": StudentModule},
 			debug=True
 		)	
 		tornado.web.Application.__init__(self, handlers, **settings)
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render(
+	def get(self):
+		students = models.Student.objects()
+		self.render(
 			"index.html",
 			page_title = "Reporta Violencia",
-			header_text = ""
+			header_text = "",
+			students = students
 		)
 
-class AddReportHandler(tornado.web.RequestHandler):
+class AddStudentHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render(
-			"agregarReporte.html",
-			page_title="Agregar Artículos",
-			header_text="Agregar Artículos"
+			"addStudent.html",
+			page_title="Add Student",
+			header_text=""
 		)
 	def post(self):
-		links = self.get_argument('links', None)
-		#self.write(links)
+		firstName = self.get_argument('firstName', None)
+		lastName = self.get_argument('lastName', None)
+		nyuid = self.get_argument('nyuid', None)
+		gradYear = self.get_argument('gradYear', None)
+		cardid = self.get_argument('cardid', None)
+		student = models.Student(
+			firstName = firstName,
+			lastName = lastName,
+			nyuid = nyuid,
+			gradYear = gradYear,
+			cardid = cardid
+		)
+		student.save()
+		self.redirect('/')
 
-class LocationReportHandler(tornado.web.RequestHandler):
+class ViewStudentHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render(
-			"reportarLugar.html",
-			page_title="Verificar Lugar",
-			header_text="Verificar Lugar"
+			"viewStudent.html",
+			page_title="View Student",
+			header_text=""
 		)
 
-class DateReportHandler(tornado.web.RequestHandler):
-	def get(self):
+class EditStudentHandler(tornado.web.RequestHandler):
+	def get(self, sid):
+		if len(sid) < 7:
+			student = models.Student.objects.get(nyuid=sid)
+		if len(sid) > 7:
+			student = models.Student.objects.get(cardid=sid)
+		if student is None:
+			self.render("404.html", message=card_id)
 		self.render(
-			"reportarFecha.html",
-			page_title="Verificar Fecha",
-			header_text="Verificar Fecha"
+			"editStudent.html",
+			page_title="Edit Student",
+			header_text="",
+			student=student
 		)
+	def post(self, sid):
+		if len(sid) < 7:
+			student = models.Student.objects.get(nyuid=sid)
+		if len(sid) > 7:
+			student = models.Student.objects.get(cardid=sid)
+		student.firstName = self.get_argument('firstName', None)
+		student.lastName = self.get_argument('lastName', None)
+		student.nyuid = self.get_argument('nyuid', None)
+		student.gradYear = self.get_argument('gradYear', None)
+		student.cardid = self.get_argument('cardid', None)
+		student.save()
+		self.redirect('/')
 
-class DuplicateReportHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render(
-			"reportarDuplicados.html",
-			page_title="Eliminar Duplicados",
-			header_text="Eliminar Duplicados"
-		)
+class ReportDeleteHandler(tornado.web.RequestHandler):
+	def get(self, report_id):
+		if len(sid) < 7:
+			student = models.Student.objects.get(nyuid=sid)
+		if len(sid) > 7:
+			student = models.Student.objects.get(cardid=sid)
+		student.delete()
+		self.redirect("/")
 
-class DetailReportHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render(
-			"reportarDetalles.html",
-			page_title="Llenar Detalles de Reportes",
-			header_text="Llenar Detalles de Reportes"
+class StudentModule(tornado.web.UIModule):
+	def render(self, student):
+		return self.render_string(
+			"modules/student.html",
+			student=student,
 		)
+	def css_files(self):
+		return "/static/css/recommended.css"
+	def javascript_files(self):
+		return "/static/js/recommended.js"
 
-class SearchReportHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render(
-			"buscarReporte.html",
-			page_title="Buscar Reportes",
-			header_text="Buscar Reportes"
-		)
-
-class DownloadReportHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render(
-			"descargarReportes.html",
-			page_title="Descargar Reportes",
-			header_text="Descargar Reportes"
-		)
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
--- models.py
+++ models.py
@@ -8,56 +8,13 @@ from datetime import datetime
 import logging
 
 
-class IndividualDeath(EmbeddedDocument):
-	group = StringField(max_length=120)
-	age = IntField(min_value=0, max_value=120)
-	gender = StringField(max_length=6)
 
-class GroupDead(EmbeddedDocument):
-	group = StringField(max_length=120)
-	numDead = IntField()
-
-class Location(EmbeddedDocument):
-	country = StringField(max_length=120, required=True)
-	state = StringField(max_length=40)
-	city = StringField(max_length=80)
-	street = StringField()
-	neighborhood = StringField()
-	zipCode = IntField(max_value=100000)
-	lat = ListField(IntField(min_value=-91, max_value=91))
-	latV = IntField(min_value=-91, max_value=91)
-	lon = ListField(IntField(min_value=-91, max_value=91))
-	lonV = IntField(min_value=-181, max_value=181)
-
-class Source(EmbeddedDocument):
-	url = URLField(verify_exists=True)
-	addTime = DateTimeField(default=datetime.now())
-
-class Report(Document):
-	ts = ComplexDateTimeField(default=datetime.now())
-	loc = EmbeddedDocumentField(Location)
-	date = DateTimeField()
-	nDead = ListField(IntField()) #once a number has been inputed three times, it is saved in the numDeadVerified field.
-	nDeadV = IntField()
-	gDead = ListField(EmbeddedDocumentField(GroupDead)) #Array contains all inputs.
-	gDeadV = ListField(EmbeddedDocumentField(GroupDead)) #This array contains only those inputs that have been repeated three times.
-	iDead = ListField(EmbeddedDocumentField(IndividualDeath)) #array contains all inputs of people that died.
-	iDeadV = ListField(EmbeddedDocumentField(IndividualDeath)) #once a IndividualDead is repeated three times in the previous field, it gets saved in this array
-	bells = ListField(StringField())
-	bellsV = ListField(StringField()) #once a belligerent is entered three times in the previous field, it's saved
-	cat = ListField(StringField())
-	catV = ListField(StringField()) #once a category is mentioned three times, it goes in here.
-	tag = ListField(StringField())
-	tagV = ListField(StringField()) #once a category is mentioned three times, it goes in here.
-	sources = ListField(EmbeddedDocumentField(Source))
-
-class UnverifiedReport(Document):
-	ts = ComplexDateTimeField(default=datetime.now())
-	loc = ListField(EmbeddedDocumentField(Location))
-	date = ListField(DateTimeField())
-	source = EmbeddedDocumentField(Source)
-
-
+class Student(Document):
+	firstName = StringField()
+	lastName = StringField()
+	nyuid = StringField()
+	cardid = IntField()
+	gradYear = IntField()
 
 
 
